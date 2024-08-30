e256a870c74e7ac95f1c3965cc53b0494cea9301
Caio Duque <147485273+cai0duque@users.noreply.github.com>
2024-08-30 06:42:06 -0300

    Atualização 8

--- Sistema de Controle de Estoque/src/com/lojaeletronicos/modelo/PagamentoPix.java
+++
@@ -1,9 +0,0 @@
-package com.lojaeletronicos.modelo;
-
-public class PagamentoPix implements Pagamento {
-
-    @Override
-    public double calcularValor(double valorOriginal) {
-        return valorOriginal; // Valor integral
-    }
-}
